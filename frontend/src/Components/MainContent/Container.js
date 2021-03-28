5700c044305176d4530d5454ca726e33ddcf4c00
Alexander Torrico <torrico.torrico.alexander@gmail.com>
2021-03-28 00:31:23 -0400

    Drawer Basico

--- frontend/src/Components/MainContent/Container.js
+++ frontend/src/Components/MainContent/Container.js
@@ -3,6 +3,7 @@ import { Box} from "@chakra-ui/react";
 import "./../../Assets/Css/Estilo.css";
 import SideBar from '../SideBar/SideBar';
 import Nav from '../SideBar/Nav';
+import LanguageView from '../languages/language_view.js'
 
 class Container extends Component{   
     render(){ 
@@ -16,7 +17,7 @@ class Container extends Component{
                         <Box h="full">
                             <Nav></Nav>
                             <Box className="Main" bg="lightblue">
-
+                                <LanguageView />
                             </Box>
                         </Box>   
                  </Box>
